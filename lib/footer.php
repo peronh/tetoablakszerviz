406a3f3c0248995e8a78305e0e5588af854acbb6
ronyecz.peter <ronyecz.peter@everest.hu>
2022-06-14 14:32:59 +0200

    initial commit

--- lib/footer.php
+++ lib/footer.php
@@ -35,7 +35,8 @@
                                 <li>
                                     Elérhetőségek
                                     <ul>
-                                        <li class="mt-3"><i class="fa fa-map-marker"></i><a href="#" aria-disabled="true">4281 Létavértes </a></li>
+                                        <li class="mt-3"><i class="fa fa-user"></i><a href="#" aria-disabled="true">Zolnai Lajos egyéni vállalkozó </a></li>
+                                        <li><i class="fa fa-map-marker"></i><a href="#" aria-disabled="true">4281 Létavértes </a></li>
                                         <li><i class="fa fa-phone"></i><a href="tel:+36203164611">+36 20 316 4611</a></li>
                                         <li><i class="fa fa-envelope"></i><a href="mailto:info@tetoteriablakszerviz.hu">info@tetoteriablakszerviz.hu</a></li>
                                     </ul>
